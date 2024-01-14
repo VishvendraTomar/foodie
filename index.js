34bdcbb543dce23f970d4b0746723fc76275aada
Vishvendra Tomar <95501800+VishvendraTomar@users.noreply.github.com>
2024-01-15 03:40:09 +0530

    mod

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { userRouter } = require("./routes/user.routes");
 const { noteRouter } = require("./routes/note.routes");
 const app = express()
 app.use(express.json())
+app.get("/",(req,res)=>{
+    res.send("welcome to home page ")
+})
 app.use("/users",userRouter)
 app.use("/notes",noteRouter)
 app.listen(8080,async()=>{
