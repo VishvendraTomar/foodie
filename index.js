e0ccfabd8e32fc925b5094864e6e5ff56fbd1229
Vishvendra Tomar <vishvendratomar29@gmail.com>
2024-01-15 05:04:07 +0530

    df

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { noteRouter } = require("./routes/note.routes");
 const cors = require("cors")
 const app = express()
 app.use(express.json())
-app.use(cors)
+app.use(cors());
 app.use("/users",userRouter)
 app.use("/notes",noteRouter)
 app.listen(8080,async()=>{
